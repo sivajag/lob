f2784ce09eeb87e88c1f0e219104e96ade80817e
Siva Jagadeesan <sivajag@gmail.com>
2010-10-22 14:55:41 -0700

    Adding initial script

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-bob "1.0.0-SNAPSHOT"
+(defproject clj-bob "0.0.1-SNAPSHOT"
   :description "Bob - Always eager to impress and help team on projects"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
