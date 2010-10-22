14d3ce75317f6c0e93d51008bfe566e6284bbb5f
Siva Jagadeesan <sivajag@gmail.com>
2010-10-22 14:35:57 -0700

    added swank depencies

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject clj-bob "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
+  :description "Bob - Always eager to impress and help team on projects"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
+                 [org.clojure/clojure-contrib "1.2.0"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]])
