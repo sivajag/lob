6ec86dbca7ae1a054ac591009cad9d50ce2cb5a2
Siva Jagadeesan <sivajag@gmail.com>
2010-10-22 15:48:41 -0700

    Added help implmentation

---
+++ src/com/sivajag/clj_bob/core.clj
@@ -0,0 +1,6 @@
+(ns com.sivajag.clj-bob.core
+  (:use [com.sivajag.clj-bob.help])
+  (:gen-class))
+
+(defn -main []
+  (help))
---
+++ src/com/sivajag/clj_bob/help.clj
@@ -0,0 +1,7 @@
+(ns com.sivajag.clj-bob.help)
+
+(defn help []
+  (println "I am Bob - I am always eager to impress and help team on projects")
+  (println "I can do these following tasks to help you")
+  
+  (println "Fire me up !!!"))
