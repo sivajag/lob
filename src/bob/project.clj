b28c6b5899b2942169e17706cf050e4065cec949
Siva Jagadeesan <sivajag@gmail.com>
2010-12-11 15:42:27 -0800

    Added functionality to add dev deps

--- src/bob/project.clj
+++ src/bob/project.clj
@@ -1,11 +1,12 @@
 (ns bob.project
-  (:require [leiningen.new :as lein-new]))
+  (:require [leiningen.new :as lein-new]
+            [leiningen.core :as lein-core]
+            [leiningen.add :as lein-add]))
 
 (defn new [name]
   (lein-new/new name))
 
-
-(defn add-dev-dep [project-path deps])
-
+(defn add-dev-deps [project deps]
+  (map (fn [[a v]] (lein-add/add project "--dev" a v)) deps))
 
 (defn add-deps [deps])
