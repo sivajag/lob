3723b29c1c511fe7aa93a0c61e1f849e0f0e6be2
Siva Jagadeesan <sivajag@gmail.com>
2010-12-11 23:13:31 -0800

    adding dependencies

--- packages/compojure.clj
+++ packages/compojure.clj
@@ -3,20 +3,19 @@
 
 (def version "1.0")
 
-(def run-deps {"org.clojure/clojure"  "1.2.0"
-               "org.clojure/clojure-contrib" "1.2.0"
-               "ring/ring-jetty-adapter" "0.2.5"
+(def run-deps {"ring/ring-jetty-adapter" "0.2.5"
                "compojure" "0.4.1"
                "hiccup" "0.2.6"
                "sandbar" "0.3.0-SNAPSHOT"})
 
 (def dev-deps {"swank-clojure" "1.3.0-SNAPSHOT"})
 
-
 (defn new 
   "Creates a new compojure project to get started with"
   [name]
-  (project/new name))
+  (project/new name)
+  (project/add-dev-deps (project/project-clj name) dev-deps)
+  (project/add-deps (project/project-clj name) run-deps))
 
 (defn add 
   "Generate new route or model to compojure project"
--- src/bob/project.clj
+++ src/bob/project.clj
@@ -1,4 +1,5 @@
 (ns bob.project
+  (:use [clojure.contrib.duck-streams :only [pwd]])
   (:require [leiningen.new :as lein-new]
             [leiningen.core :as lein-core]
             [leiningen.add :as lein-add]))
@@ -6,7 +7,14 @@
 (defn new [name]
   (lein-new/new name))
 
+(defn project-clj [project-name]
+  (lein-core/read-project (str (pwd) "/" project-name "/project.clj")))
+
+(defn current-project-clj []
+  (lein-core/read-project (str (pwd) "/project.clj")))
+
 (defn add-dev-deps [project deps]
   (map (fn [[a v]] (lein-add/add project "--dev" a v)) deps))
 
-(defn add-deps [deps])
+(defn add-deps [project deps]
+  (map (fn [[a v]] (lein-add/add project a v)) deps))
