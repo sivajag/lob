286fb83f4881b03060efdac3afe99b1aec03ae77
Siva Jagadeesan <sivajag@gmail.com>
2010-12-25 04:46:20 -0800

    using clojure controb string

--- src/bob/utils/ns.clj
+++ src/bob/utils/ns.clj
@@ -1,5 +1,6 @@
 (ns bob.utils.ns
-  (:require [clojure.contrib.find-namespaces :as find-ns]))
+  (:require [clojure.contrib.find-namespaces :as find-ns]
+            [clojure.contrib.string :as contrib-str]))
 
 (defn tasks-in [nz]
   (filter #(let [m (meta %)] (contains? m :task))
@@ -9,7 +10,7 @@
   (str (:ns (meta f))))
 
 (defn ns-last-str [f]
-  (last (clojure.string/split (ns-str f) #"\.")))
+  (last (contrib-str/split #"\." (ns-str f))))
 
 (defn namespaces-matching [prefix]
   (filter #(.startsWith (name %) prefix) 
